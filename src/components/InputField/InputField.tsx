09a56f6429e23f5856ebd36d1def0bbf6df266b6
Stameskaaa <chta02@mail.ru>
2025-01-22 11:16:43 +0500

    add input focus

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { ArrowIcon } from '../../icons/arrow';
 import styles from './inputFiled.module.css';
 import { TaskList, Task } from '../../types/types';
@@ -10,6 +10,7 @@ interface Props {
 
 export const InputField: React.FC<Props> = ({ setList }) => {
   const [task, setTask] = useState<Task>({ text: '', state: 'active', id: '' });
+  const inputRef = useRef<HTMLInputElement>(null);
 
   function generateTimedID() {
     const timestamp = Date.now().toString(36);
@@ -26,6 +27,7 @@ export const InputField: React.FC<Props> = ({ setList }) => {
     const id = generateTimedID();
     setList((list) => [...list, { ...task, id }]);
     setTask({ text: '', state: 'active', id: '' });
+    if (inputRef.current) inputRef.current.focus();
   }
 
   function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
@@ -40,6 +42,7 @@ export const InputField: React.FC<Props> = ({ setList }) => {
         <ArrowIcon />
       </button>
       <input
+        ref={inputRef}
         value={task.text}
         onKeyDown={handleKeyDown}
         onChange={handleTaskInput}
